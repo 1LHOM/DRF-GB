217dc3e4f57c25a5ec3dd778e212b607c088f7b9
1LHOM <75044532+1LHOM@users.noreply.github.com>
2022-02-22 11:55:51 +0300

    library app replaced with todo_users app

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from library.views import AuthorModelViewSet
+from todo_users.views import ToDoUserViewSet
 
 router = DefaultRouter()
-router.register('lib_authors', AuthorModelViewSet)
+router.register('todo_users', ToDoUserViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- backend/todo_users/serializers.py
+++ backend/todo_users/serializers.py
@@ -1,8 +1,8 @@
 from rest_framework.serializers import HyperlinkedModelSerializer
-from .models import Author
+from .models import ToDoUser
 
 
 class AuthorModelSerializer(HyperlinkedModelSerializer):
     class Meta:
-        model = Author
-        fields = ('username', 'first_name', 'last_name', 'email', 'birthday_year')
+        model = ToDoUser
+        fields = ('username', 'first_name', 'last_name', 'email')
